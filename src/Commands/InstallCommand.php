70222025c636951d0b13e34780983c21c30153eb
altynbek <altynbek@rocketfirm.com>
2019-04-10 12:33:48 +0600

    feat: add InstallCommand

---
+++ src/Commands/InstallCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace rocketfirm\rdrive\commands;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'rdrive:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install the Rocket Drive package';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Display this on the screen');
+    }
+}
