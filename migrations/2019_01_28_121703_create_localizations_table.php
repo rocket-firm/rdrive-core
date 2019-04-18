b64f861b17ffc1aa94d9022cdccac0d46d5dff10
altynbek07 <altynbek.kazezov.97@gmail.com>
2019-04-18 11:31:36 +0600

    feat: add migrations

---
+++ migrations/2019_01_28_121703_create_localizations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocalizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('localizations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('item_id');
+            $table->string('lang', 32);
+            $table->string('group');
+            $table->string('key');
+            $table->text('value')->nullable();
+
+            $table->unique(['item_id', 'lang', 'group', 'key']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('localizations');
+    }
+}
---
+++ migrations/2019_01_30_050940_create_settings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('item_id');
+            $table->string('lang', 32);
+            $table->string('group');
+            $table->string('key');
+            $table->string('name');
+            $table->text('value')->nullable();
+            $table->string('type');
+
+            $table->unique(['item_id', 'lang', 'group', 'key']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
