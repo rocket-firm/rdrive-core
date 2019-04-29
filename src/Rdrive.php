7a6326cf749a58565c1dd42b4910fdb67f3008be
lipricon01 <kim.vlad32@gmail.com>
2019-04-29 11:45:42 +0600

    feat: add new function to check and/or overwrite .env Token

--- src/Rdrive.php
+++ src/Rdrive.php
@@ -2,6 +2,15 @@
 
 namespace Rocketfirm\Rdrive;
 
+use Sven\FlexEnv\Env;
+
 class Rdrive
 {
+
+    public function checkToken($key, $value)
+    {
+        $env = new Env(base_path('.env'));
+        $env->set($key, $value);
+        return true;
+    }
 }
