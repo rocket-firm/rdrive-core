c1e9e71ecda963e03b36558ed2e0111a56760ce5
altynbek07 <altynbek.kazezov.97@gmail.com>
2019-05-14 16:38:53 +0500

    fix: index layout

--- publishable/resources/views/index.blade.php
+++ publishable/resources/views/index.blade.php
@@ -5,12 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>{{ config('app.name', 'Laravel') }}</title>
+
     <!-- Scripts -->
-    <script src="{{ asset('js/admin/index.js') }}" defer></script>
+    <script src="{{ asset('admin/js/app.min.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('admin/css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <h1>Test</h1>
     <div id="app">
     </div>
 </body>
