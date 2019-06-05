37f58626031a364d49e62c0134a7875f22d36677
Yerlan Yeszhanov <yerlanyeszhanov@iMac.local>
2019-06-05 18:11:40 +0600

    added: switch slider

---
+++ resources/js/components/ui/Switch.jsx
@@ -0,0 +1,66 @@
+
+import React from 'react';
+import styled from 'styled-components'
+
+const SwitchInputUI = styled.input.attrs({ type: 'checkbox' })`
+    opacity: 0;
+    width: 0;
+    height: 0;
+    
+`
+const SwitchUI =styled.label`
+    position: relative;
+    display: inline-block;
+    width: 22px;
+    height: 12px;
+    margin-bottom: 0;
+    vertical-align: middle;
+    ${SwitchInputUI}:checked + ${SwitchSliderUI}{
+        background-color: #737A9B; 
+    }
+     ${SwitchInputUI}:checked + ${SwitchSliderUI}:before{
+        transform: translateX(10px);
+    }
+   
+`
+
+const SwitchSliderUI= styled.span`
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color:#BBCDD9;
+    transition: .4s;
+    border-radius: 10px;
+   
+    &:before {
+        content: "";
+        position: absolute;
+        width: 10px;
+        height: 10px;
+        left: 1px;
+        bottom: 1px;
+        background-color: #FFF;
+        transition: .4s;
+        border-radius: 50%;
+    }
+    
+`
+
+const Switch = ({
+  toggle,IsToggled, ...attrs
+}) => {
+
+  return (
+      <SwitchUI> 
+          <SwitchInputUI/>
+          <SwitchSliderUI/>
+      </SwitchUI>
+  );
+};
+
+
+
+export default Switch;
--- resources/js/screens/Kit.jsx
+++ resources/js/screens/Kit.jsx
@@ -6,11 +6,11 @@ import Input from 'components/ui/Input'
 import Sidebar from 'components/ui/Sidebar' 
 import links from "../mockup/links"
 import Checkbox from "components/ui/Checkbox"
-
+import Switch from "components/ui/Switch"
 
 
 const Kit = () => {
-  const [count, setCount] = useState(false)
+  const [isChecked, setChecker] = useState(false)
 
  return (
    <Layout vertical>
@@ -19,11 +19,12 @@ const Kit = () => {
     <Input />
     <label>
       <Checkbox
-        checked={count}
-        onChange={()=>setCount(event.target.checked)}
+        checked={isChecked}
+        onChange={()=>setChecker(event.target.checked)}
       />
       <span style={{ marginLeft: 8 }}>Label Text</span>
     </label>
+    <Switch/>
     <Sidebar title='Kazkommerts Securities'  data={links} />
     
   </Layout>
